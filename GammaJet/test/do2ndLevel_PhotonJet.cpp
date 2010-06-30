fcc9f7f28f58b4c69c74be8b7472deb17b886582
pandolf <>
2010-06-30 15:57:34 +0000

    now usegenjets passed from command line

--- GammaJet/test/do2ndLevel_PhotonJet.cpp
+++ GammaJet/test/do2ndLevel_PhotonJet.cpp
@@ -12,8 +12,8 @@ void doSingleLoop(std::string name, std::string recoType, std::string jetAlgo, b
 int main(int argc, char* argv[]) { 
 //do2ndLevel(std::string dataset, std::string recoType, std::string jetAlgo) {
 
-  if( argc != 4 ) {
-    std::cout << "USAGE: ./do2ndLevel_PhotonJet [dataset] [recoType] [jetAlgo]" << std::endl;
+  if( argc != 4 &&  argc != 5  ) {
+    std::cout << "USAGE: ./do2ndLevel_PhotonJet [dataset] [recoType] [jetAlgo] [useGenJets=false]" << std::endl;
     exit(23);
   }
 
@@ -22,8 +22,12 @@ int main(int argc, char* argv[]) {
   std::string jetAlgo(argv[3]);
 
   std::string name;
-  bool useGenJets = true;
-    
+  bool useGenJets = false;
+  if( argc == 5 ) {
+    std::string useGenJets_str(argv[4]);
+    if( useGenJets_str=="true" )
+      useGenJets = true;
+  }
     
   if( dataset=="PhotonJet_Summer09" || dataset=="QCD_Summer09") {
 
@@ -44,24 +48,58 @@ int main(int argc, char* argv[]) {
     name = dataset + "_Pt1400";
     doSingleLoop(name, recoType, jetAlgo, (bool)false, useGenJets);
 
-  } else if( dataset=="PhotonJet_Spring10" || dataset=="QCD_Spring10" ) {
+  } else if( dataset=="PhotonJet_Spring10" ) {
     
     name = dataset + "_Pt0to15";
 //    doSingleLoop(name, recoType, jetAlgo, (bool)false, (bool)false);
     name = dataset + "_Pt15";
-    doSingleLoop(name, recoType, jetAlgo, (bool)false, (bool)false);
+    doSingleLoop(name, recoType, jetAlgo, (bool)false, useGenJets);
     name = dataset + "_Pt30";
-    doSingleLoop(name, recoType, jetAlgo, (bool)false, (bool)false);
+    doSingleLoop(name, recoType, jetAlgo, (bool)false, useGenJets);
     name = dataset + "_Pt80";
-    doSingleLoop(name, recoType, jetAlgo, (bool)false, (bool)false);
+    doSingleLoop(name, recoType, jetAlgo, (bool)false, useGenJets);
     
-  } else if( dataset=="MinimumBias_Commissioning10-GOODCOLL-v9" || dataset=="MinimumBias_Commissioning10-Apr20Skim_GOODCOLL-v1" ) {
+  } else if( dataset=="QCD_Spring10" ) {
+    
+    name = dataset + "_Pt0to15";
+    doSingleLoop(name, recoType, jetAlgo, (bool)false, useGenJets);
+    name = dataset + "_Pt15to20";
+    doSingleLoop(name, recoType, jetAlgo, (bool)false, useGenJets);
+    name = dataset + "_Pt20to30";
+    doSingleLoop(name, recoType, jetAlgo, (bool)false, useGenJets);
+    name = dataset + "_Pt30to50";
+    doSingleLoop(name, recoType, jetAlgo, (bool)false, useGenJets);
+    name = dataset + "_Pt30to50";
+    doSingleLoop(name, recoType, jetAlgo, (bool)false, useGenJets);
+    name = dataset + "_Pt50to80";
+    doSingleLoop(name, recoType, jetAlgo, (bool)false, useGenJets);
+    name = dataset + "_Pt80";
+    doSingleLoop(name, recoType, jetAlgo, (bool)false, useGenJets);
+    name = dataset + "_Pt170";
+    doSingleLoop(name, recoType, jetAlgo, (bool)false, useGenJets);
+    name = dataset + "_Pt300";
+    doSingleLoop(name, recoType, jetAlgo, (bool)false, useGenJets);
+    name = dataset + "_Pt470";
+    doSingleLoop(name, recoType, jetAlgo, (bool)false, useGenJets);
+    name = dataset + "_Pt800";
+    doSingleLoop(name, recoType, jetAlgo, (bool)false, useGenJets);
+    name = dataset + "_Pt1400";
+    doSingleLoop(name, recoType, jetAlgo, (bool)false, useGenJets);
 
-    doSingleLoop(dataset, recoType, jetAlgo, (bool)true, (bool)false);
+  } else if( dataset=="MinimumBias_Commissioning10-GOODCOLL-v9" 
+          || dataset=="MinimumBias_Commissioning10-Apr20Skim_GOODCOLL-v1" 
+          || dataset=="MinimumBias_Commissioning10_May6thPDSkim2_SD_EG" 
+          || dataset=="MinimumBias_Commissioning10_SD_EG-v9"
+          || dataset=="EG_Run2010A-PromptReco-v1"
+          || dataset=="EG_Run2010A-PromptReco-v2"
+          || dataset=="EG_Run2010A-PromptReco-v4"
+          ) {
+
+    doSingleLoop(dataset, recoType, jetAlgo, (bool)true, useGenJets);
 
   } else { //generic case:
 
-    doSingleLoop(dataset, recoType, jetAlgo, (bool)false, (bool)false);
+    doSingleLoop(dataset, recoType, jetAlgo, (bool)false, useGenJets);
 
   }
 
@@ -73,9 +111,10 @@ void doSingleLoop(std::string name, std::string recoType, std::string jetAlgo, b
   std::cout << "Starting: " << name << std::endl;
   TreeAnalyzer_PhotonJet* t = new TreeAnalyzer_PhotonJet(name.c_str(), recoType, jetAlgo, useGenJets);
   if( useJSON ) {
-    t->ReadJSONFile("json_L1_HLT_PIX_STRIP_ECAL_HCAL_132440_135175.txt");
-    t->ReadCSVFile("lumi_by_LS_all.csv");
+    t->ReadJSONFile("Cert_132440-137028_7TeV_StreamExpress_Collisions10_CMSSWConfig.txt");
+    t->ReadCSVFile("lumi_by_LS_upto137028.csv");
   }
+  t->LoadInput();
   t->Loop();
   delete t;
 
